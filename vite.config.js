6d92aedd61197b54fa3df99b7b727447c8f05a3b
NicoAroyo <nico.aroyo@gmail.com>
2023-07-19 12:57:00 +0300

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
-    base: "/nicorgi",
+    base: "/nicorgi/",
   };
 
   return config;
